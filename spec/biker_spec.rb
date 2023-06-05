cca0d92fc7b25ddec79a302e3f58a55ee15dc24a
Allen Russell <garyrusselljr@gmail.com>
2023-06-05 09:33:01 -0600

    test: Add log_ride test

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -27,4 +27,18 @@ RSpec.describe Biker do
       expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
     end
   end
+
+  describe "#log_ride" do
+    it "can log rides" do
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.rides).to eq({
+        @ride1 => [92.5, 91.1],
+        @ride2 => [60.9, 61.6]
+      })
+    end
+  end
 end
