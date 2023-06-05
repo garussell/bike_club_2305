ad03e1548152b26c9798a73bcd08ac031c699176
Allen Russell <garyrusselljr@gmail.com>
2023-06-05 09:18:55 -0600

    feat: Add test for total_distance and refactored loop? test

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -12,7 +12,20 @@ RSpec.describe Ride do
       expect(@ride1.name).to eq("Walnut Creek Trail")
       expect(@ride1.distance).to eq(10.7)
       expect(@ride1.terrain).to eq(:hills)
+    end
+  end
+  
+  describe "#loop?" do
+    it "can tell you if the ride loops" do
       expect(@ride1.loop?).to be false
+      expect(@ride2.loop?).to be true
+    end
+  end
+
+  describe "#total_distance" do
+    it "can tell you the total distance" do
+      expect(@ride1.total_distance).to eq(21.4)
+      expect(@ride2.total_distance).to eq(14.9)
     end
   end
 end
