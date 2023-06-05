f5d01cd286ade48c4263e963ad867de6a17c628c
Allen Russell <garyrusselljr@gmail.com>
2023-06-05 11:34:49 -0600

    test: Add test for fastest_time method

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -10,6 +10,17 @@ RSpec.describe BikeClub do
     # rides
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    # add bikers
+    @bike_club.add_biker(@biker)
+    @bike_club.add_biker(@biker2)
+    @bike_club.add_biker(@biker3)
+    # bikers know the terrain
+    @biker.learn_terrain!(:gravel)
+    @biker.learn_terrain!(:hills)
+    @biker2.learn_terrain!(:gravel)
+    @biker2.learn_terrain!(:hills)
+    @biker3.learn_terrain!(:gravel)
+    @biker3.learn_terrain!(:hills)
   end
 
   describe "#initialize" do
@@ -22,25 +33,11 @@ RSpec.describe BikeClub do
 
   describe "#add_biker" do
     it "can add bikers to the club" do
-      @bike_club.add_biker(@biker)
-      @bike_club.add_biker(@biker2)
-      @bike_club.add_biker(@biker3)
-
       expect(@bike_club.bikers).to eq([@biker, @biker2, @biker3])
     end
   end
   describe "#logged_most_rides" do
     it "can tell us which Biker has logged the most rides" do
-      @bike_club.add_biker(@biker)
-      @bike_club.add_biker(@biker2)
-      @bike_club.add_biker(@biker3)
-      # bikers know the terrain
-      @biker.learn_terrain!(:gravel)
-      @biker.learn_terrain!(:hills)
-      @biker2.learn_terrain!(:gravel)
-      @biker2.learn_terrain!(:hills)
-      @biker3.learn_terrain!(:gravel)
-      @biker3.learn_terrain!(:hills)
       #bikers log some rides
       @biker.log_ride(@ride1, 92.5)
       @biker.log_ride(@ride1, 91.1)
@@ -51,6 +48,22 @@ RSpec.describe BikeClub do
       @biker3.log_ride(@ride2, 65.5)
 
       expect(@bike_club.logged_most_rides).to eq(@biker3)
+    end
+  end
+
+  describe "#fastest_time" do
+    it "can tell us which Biker has the best time for a given Ride" do
+      # ride1
+      @biker.log_ride(@ride1, 91.1)
+      @biker2.log_ride(@ride1, 97.0)
+      @biker3.log_ride(@ride1, 91.0)
+      # ride2
+      @biker.log_ride(@ride2, 64.7)
+      @biker1.log_ride(@ride2, 67.0)
+      @biker3.log_ride(@ride2, 65.5)
+
+      expect(@bike_club.fastest_time(@ride1)).to eq(@biker3)
+      expect(@bike_club.fastest_time(@ride2)).to eq(@biker)
     end
   end
 end
