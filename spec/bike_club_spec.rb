844bd8ae4533f2ca56ea5a25619d49c3fd0dab4c
Allen Russell <garyrusselljr@gmail.com>
2023-06-05 11:05:00 -0600

    test: Added tests for add_biker and logged_most_rides

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe BikeClub do
     @biker = Biker.new("Kenny", 30)
     @biker2 = Biker.new("Athena", 15)
     @biker3 = Biker.new("Philipe", 45)
+    # rides
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
   describe "#initialize" do
@@ -17,4 +20,34 @@ RSpec.describe BikeClub do
     end
   end
 
+  describe "#add_biker" do
+    it "can add bikers to the club" do
+      @biker.add_biker
+      @biker2.add_biker
+      @biker3.add_biker
+
+      expect(@bike_club.bikers).to eq([@biker, @biker2, @biker3])
+    end
+  end
+  describe "#logged_most_rides" do
+    it "can tell us which Biker has logged the most rides" do
+      # bikers know the terrain
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+      @biker2.learn_terrain!(:gravel)
+      @biker2.learn_terrain!(:hills)
+      @biker3.learn_terrain!(:gravel)
+      @biker3.learn_terrain!(:hills)
+      #bikers log some rides
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker2.log_ride(@ride1, 97.0)
+      @biker2.log_ride(@ride2, 67.0)
+      @biker3.log_ride(@ride1, 104.3)
+      @biker3.log_ride(@ride1, 91.0)
+      @biker3.log_ride(@ride2, 65.5)
+
+      expect(@bike_club.logged_most_rides).to eq(@biker3)
+    end
+  end
 end
