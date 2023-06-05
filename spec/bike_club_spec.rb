30fe80130324f44f491f8fcd5c75882f8eef7aea
Allen Russell <garyrusselljr@gmail.com>
2023-06-05 11:54:26 -0600

    fix: Issue with test

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -10,10 +10,6 @@ RSpec.describe BikeClub do
     # rides
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
-    # add bikers
-    @bike_club.add_biker(@biker)
-    @bike_club.add_biker(@biker2)
-    @bike_club.add_biker(@biker3)
     # bikers know the terrain
     @biker.learn_terrain!(:gravel)
     @biker.learn_terrain!(:hills)
@@ -33,11 +29,20 @@ RSpec.describe BikeClub do
 
   describe "#add_biker" do
     it "can add bikers to the club" do
+      # add bikers
+      @bike_club.add_biker(@biker)
+      @bike_club.add_biker(@biker2)
+      @bike_club.add_biker(@biker3)
+
       expect(@bike_club.bikers).to eq([@biker, @biker2, @biker3])
     end
   end
   describe "#logged_most_rides" do
     it "can tell us which Biker has logged the most rides" do
+      # add bikers
+      @bike_club.add_biker(@biker)
+      @bike_club.add_biker(@biker2)
+      @bike_club.add_biker(@biker3)
       #bikers log some rides
       @biker.log_ride(@ride1, 92.5)
       @biker.log_ride(@ride1, 91.1)
@@ -53,13 +58,17 @@ RSpec.describe BikeClub do
 
   describe "#fastest_time" do
     it "can tell us which Biker has the best time for a given Ride" do
+      # add bikers
+      @bike_club.add_biker(@biker)
+      @bike_club.add_biker(@biker2)
+      @bike_club.add_biker(@biker3)
       # ride1
       @biker.log_ride(@ride1, 91.1)
       @biker2.log_ride(@ride1, 97.0)
       @biker3.log_ride(@ride1, 91.0)
       # ride2
       @biker.log_ride(@ride2, 64.7)
-      @biker1.log_ride(@ride2, 67.0)
+      @biker2.log_ride(@ride2, 67.0)
       @biker3.log_ride(@ride2, 65.5)
 
       expect(@bike_club.fastest_time(@ride1)).to eq(@biker3)
