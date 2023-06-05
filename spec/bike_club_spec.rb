a81fa631debe87b8b6230baebfc0a36869248900
Allen Russell <garyrusselljr@gmail.com>
2023-06-05 10:55:44 -0600

    test: Add files and test for initialize BikeClub class

---
+++ spec/bike_club_spec.rb
@@ -0,0 +1,19 @@
+require './spec/spec_helper'
+
+RSpec.describe BikeClub do
+  before(:each) do
+    @bike_club = BikeClub.new('Big Wheels')
+    # bikers
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @biker3 = Biker.new("Philipe", 45)
+  end
+
+  describe "#initialize" do
+    it "exists and has attributes" do
+      expect(@bike_club).to be_an_instance_of(BikeClub)
+      expect(@bike_club.bikers).to eq([])
+    end
+  end
+  
+end
