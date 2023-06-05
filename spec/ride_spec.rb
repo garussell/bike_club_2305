50c94cefeb548497c0ab46eb1ae95e2202d79f23
Allen Russell <garyrusselljr@gmail.com>
2023-06-05 09:16:06 -0600

    feat: Add initialize and loop? methods

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -1,6 +1,6 @@
 require './spec/spec_helper'
 
-RSpec.describe Rides do
+RSpec.describe Ride do
   before(:each) do
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
@@ -12,7 +12,7 @@ RSpec.describe Rides do
       expect(@ride1.name).to eq("Walnut Creek Trail")
       expect(@ride1.distance).to eq(10.7)
       expect(@ride1.terrain).to eq(:hills)
-      
+      expect(@ride1.loop?).to be false
     end
   end
 end
